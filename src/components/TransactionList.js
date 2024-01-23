cb4a654f477fe33a0ae48962fa55b5018e420dde
Sameer Hussain <imsameer76@gmail.com>
2024-01-23 21:54:25 +0530

    Updated Responsive Code

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -9,7 +9,7 @@ export const TransactionList = () => {
   return (
     <>
     <h3 className='add'>History</h3>
-    <svg className='addnew' xmlns="http://www.w3.org/2000/svg" width="439" height="3" viewBox="0 0 439 3" fill="none">
+    <svg className='addnew' xmlns="http://www.w3.org/2000/svg" width="auto" height="3" viewBox="0 0 439 3" fill="none">
   <path d="M4.30672e-05 1.55313L438.542 1.43547" stroke="#2E2E2E"/>
 </svg>
     <ul id="list" className='list'>
